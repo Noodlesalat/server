b84a5dddecfa6945201c841c483dd8d975ddc0fc
Fabian Dreßler <nudelsalat@clouz.de>
2024-04-24 22:23:35 +0200

    fix: update last_login timestamp for token based-logins
    fixes #31075 and maybe #32953
    
    Signed-off-by: Fabian Dreßler <nudelsalat@clouz.de>

--- lib/private/User/Session.php
+++ lib/private/User/Session.php
@@ -393,6 +393,7 @@ class Session implements IUserSession, Emitter {
 		if ($isToken) {
 			$this->setToken($loginDetails['token']->getId());
 			$this->lockdownManager->setToken($loginDetails['token']);
+			$user->updateLastLoginTimestamp();
 			$firstTimeLogin = false;
 		} else {
 			$this->setToken(null);
